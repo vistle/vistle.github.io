f2f596e19a9cda3e0bc5b8fb7288d620a0e0ce91
Martin Aumüller <aumuell@reserv.at>
2025-05-19 22:56:59 +0200

    let's hope that all requirements are already met, if there is no internet

--- sphinx/build.sh
+++ sphinx/build.sh
@@ -5,7 +5,8 @@ VENV=$DIR/venv
 uv venv $VENV
 #VIRTUAL_ENV=$VENV uv --preview pip install .[docs,pandas,flask,fastapi,rdflib,sphinx]
 export VIRTUAL_ENV=$VENV
-uv pip install -r $DIR/../docs/requirements.txt
+ping -c 1 www.python.org && \
+    uv pip install -r $DIR/../docs/requirements.txt
 source $VENV/bin/activate
 #python -m sphinx -T -b html -d docs/_build/doctrees -D language=en docs/source $READTHEDOCS_OUTPUT/html
 if [ -n "$*" ]; then
