eeb61628e0e64fd88e765917c32f43df3e64d443
Martin Aumüller <aumuell@reserv.at>
2026-04-09 09:16:08 +0200

    require sphinx 8.2
    9 does not seem to work

--- sphinx/build.sh
+++ sphinx/build.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
 DIR=$(dirname "$0")
-VENV=$DIR/venv
+VENV="$DIR/venv"
 export UV_VENV_CLEAR=1
-uv venv $VENV
+uv venv "$VENV"
 #VIRTUAL_ENV=$VENV uv --preview pip install .[docs,pandas,flask,fastapi,rdflib,sphinx]
-export VIRTUAL_ENV=$VENV
+export VIRTUAL_ENV="$VENV"
 ping -c 1 www.python.org && \
-    uv pip install -r $DIR/../docs/requirements.txt
+    uv pip install -r "$DIR/../docs/requirements.txt"
 source $VENV/bin/activate
 #python -m sphinx -T -b html -d docs/_build/doctrees -D language=en docs/source $READTHEDOCS_OUTPUT/html
 if [ -n "$*" ]; then
     python -m sphinx "$@"
 else
-    rm -r "$DIR/../html/"
-    python -m sphinx $DIR/../docs $DIR/../html
+    docdir="$DIR/../html/"
+    test -d "$docdir" && rm -r "$docdir"
+    python -m sphinx "$DIR/../docs" "$docdir"
 fi
